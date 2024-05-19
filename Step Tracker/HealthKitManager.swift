d967f02e99698a6bbba618f908c166d5552f0c2f
Shadat Rahman <shadat.rahman.464@gmail.com>
2024-05-19 23:14:37 +0600

    step bar chart styling

--- Step Tracker/HealthKitManager.swift
+++ Step Tracker/HealthKitManager.swift
@@ -25,10 +25,13 @@ import Observation
             anchorDate: endDate,
             intervalComponents: .init(day: 1)
         )
-        
-        let stepsCount = try! await stepsQuery.result(for: store)
-        stepData = stepsCount.statistics().map{
-            .init(date: $0.startDate, value: $0.sumQuantity()?.doubleValue(for: .count()) ?? 0)
+        do {
+            let stepsCount = try await stepsQuery.result(for: store)
+            stepData = stepsCount.statistics().map{
+                .init(date: $0.startDate, value: $0.sumQuantity()?.doubleValue(for: .count()) ?? 0)
+            }
+        } catch {
+            
         }
     }
     
@@ -46,11 +49,16 @@ import Observation
             anchorDate: endDate,
             intervalComponents: .init(day: 1)
         )
-        
-        let weights = try! await weightQuery.result(for: store)
-        weightData = weights.statistics().map {
-            .init(date: $0.startDate, value: $0.mostRecentQuantity()?.doubleValue(for: .pound()) ?? 0)
+        do {
+            let weights = try await weightQuery.result(for: store)
+            weightData = weights.statistics().map {
+                .init(date: $0.startDate, value: $0.mostRecentQuantity()?.doubleValue(for: .pound()) ?? 0)
+            }
+        } catch {
+            
         }
+            
+        
     }
     
 //    func addSimulatorDate() async {
